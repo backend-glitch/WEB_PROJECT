155187296520ffdcf880ea5b889f6259b98deb1b
Arjun_2025 <vineetaarjun123@gmail.com>
2025-12-25 17:56:00 +0530

    25-12-2025

--- JS_Files/shapes.js
+++ JS_Files/shapes.js
@@ -8,14 +8,20 @@ const gradients = [
   "linear-gradient(135deg, #6a5af9, #b45eff)",
   "linear-gradient(135deg, #4dd0e1, #0097a7)",
   "linear-gradient(135deg, #4dff91, #24c96b)",
-  "linear-gradient(135deg, #ff6ec4, #7873f5)"
+  "linear-gradient(135deg, #ff6ec4, #7873f5)",
+   "linear-gradient(135deg, #6a5af9, #b45eff)",
+   "linear-gradient(135deg, #ff6ec4, #7873f5)"
 ];
 
 let positions = [
   { top: "10%", delay: "0s", duration: "18s" },
   { top: "40%", delay: "3s", duration: "20s" },
   { top: "70%", delay: "1s", duration: "22s" },
-  { top: "85%", delay: "5s", duration: "17s" }
+  { top: "85%", delay: "5s", duration: "17s" },
+   { top: "95%", delay: "2s", duration: "18s" },
+    { top: "15%", delay: "8s", duration: "17s" },
+     { top: "55%", delay: "12s", duration: "20s" }
+   
 ];
 
 positions.forEach(pos => {
