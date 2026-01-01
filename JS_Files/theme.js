5bf995e80cbd711133525f671bb6f19206ed8143
Arjun_2025 <vineetaarjun123@gmail.com>
2026-01-01 11:34:22 +0530

    made site responsive for mobile

--- JS_Files/theme.js
+++ JS_Files/theme.js
@@ -15,3 +15,20 @@ toggler.onclick = () => {
         toggler.textContent = "Dark";  // Show "Dark" when light theme is active
     }
 };
+
+
+// 2
+const toggler2 = document.getElementById("themeToggle2");
+//const body = document.body;
+
+toggler2.onclick = () => {
+    // Toggle dark class
+    body.classList.toggle("dark");
+
+    // Update button text based on current theme
+    if (body.classList.contains("dark")) {
+        toggler2.textContent = "☀️"; // Show "Light" when dark theme is active
+    } else {
+        toggler2.textContent = "🌙";  // Show "Dark" when light theme is active
+    }
+};
